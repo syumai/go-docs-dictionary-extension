6b4b4b563167063211a0a160a6c2b3647953c28c
task4233 <29667656+task4233@users.noreply.github.com>
2021-07-23 14:58:26 +0900

    bundle js files

--- src/app.js
+++ src/app.js
@@ -1,9 +1,77 @@
-import { collectTextNodes, processNode } from "./nodes.js";
+window.onload = () => {
+  console.log("window loaded");
+  main();
+};
 
-const textNodes = collectTextNodes();
-for (const node of textNodes) {
-  if (node.parentNode.nodeName === "PRE") {
-    continue;
+function main() {
+  const nodesToIgnore = new Set(["PRE"]);
+  const textNodes = collectTextNodes();
+  for (const node of textNodes) {
+    if (nodesToIgnore.has(node.parentNode.nodeName)) {
+      continue;
+    }
+    processNode(node);
   }
-  processNode(node);
+}
+
+function collectTextNodes() {
+  const container = document.querySelector(".container");
+  const r = [];
+  let n;
+  const walker = document.createTreeWalker(
+    container,
+    NodeFilter.SHOW_TEXT,
+    null,
+    false
+  );
+  while ((n = walker.nextNode())) {
+    r.push(n);
+  }
+  return r;
+}
+
+function lookupWord(word) {
+  const stem = word2stem[word.toLowerCase()];
+  if (!stem) {
+    return "";
+  }
+  const meaning = dic[stem];
+  if (!meaning) {
+    return "";
+  }
+  return meaning;
+}
+
+function processNode(node) {
+  const fragment = document.createDocumentFragment();
+  const words = node.textContent.split(/\s+/);
+  words.forEach((word) => {
+    const span = document.createElement("span");
+    span.textContent = word;
+    fragment.appendChild(span);
+    fragment.appendChild(document.createTextNode(" "));
+
+    const trimmedWord = word.replace(/['",.:;]/g, "");
+    const meaning = lookupWord(trimmedWord);
+    if (!meaning) {
+      return;
+    }
+    span.className = "word";
+    const tooltip = document.createElement("div");
+    tooltip.textContent = meaning;
+    tooltip.className = "word-translation";
+    span.appendChild(tooltip);
+  });
+  if (fragment.children.length > 0 && fragment.lastChild.textContent === " ") {
+    fragment.removeChild(fragment.lastChild);
+  }
+  // workaround to avoid making h2 > pre and h3 > pre
+  if (node.parentNode.nodeName == "H2" || node.parentNode.nodeName == "H3") {
+    // wrap by div
+    const div = document.createElement("div");
+    div.appendChild(fragment);
+    node.parentNode.replaceChild(div, node);
+  } else {
+    node.parentNode.replaceChild(fragment, node);
+  }
 }
